9408999ea61dc2b4aacc3d82a8f1191f0a9b0bcb
John Fercher <johnathanfercher22@gmail.com>
2019-04-10 02:35:54 -0300

    min

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,10 @@
 extern crate rand;
-
 fn main() {
-    let all_odds = vec![1, 3];
-    let not_all_odds = vec![1, 2, 3];
-
-    let has_any_odd1 = all_odds
-        .clone()
-        .into_iter()
-        .any(|x| x % 2 == 0);
-
-    let has_any_odd2 = not_all_odds
-        .clone()
-        .into_iter()
-        .any(|x| x % 2 == 0);
-
-    println!("{:?}", all_odds);
-    println!("{:?}", not_all_odds);
-    println!("{:?}", has_any_odd1);
-    println!("{:?}", has_any_odd2);
+    let colecao_aleatoria: [u16; 8] = rand::random();
+    let min = colecao_aleatoria
+        .iter()
+        .min()
+        .unwrap();
+    println!("{:?}", colecao_aleatoria);
+    println!("{:?}", min);
 }
