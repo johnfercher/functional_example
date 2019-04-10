3a94d5aedcc10654b47bf5925a7dccd3eacd98bc
John Fercher <johnathanfercher22@gmail.com>
2019-04-09 23:59:42 -0300

    max

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ extern crate rand;
 fn main() {
     let colecao_aleatoria: [u16; 8] = rand::random();
 
-    let sum = colecao_aleatoria
+    let max = colecao_aleatoria
         .iter()
         .max()
         .unwrap();
 
     println!("{:?}", colecao_aleatoria);
-    println!("{:?}", sum);
+    println!("{:?}", max);
 }
