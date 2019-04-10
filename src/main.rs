c7064043682616228ab94a7f483c05260a708638
John Fercher <johnathanfercher22@gmail.com>
2019-04-10 02:25:40 -0300

    all

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 extern crate rand;
 
 fn main() {
-    let colecao_aleatoria: [u16; 8] = rand::random();
+    let all_evens = vec![2, 4];
+    let not_all_evens = vec![2, 4, 3];
 
-    let (pares, impares): (Vec<u16>, Vec<u16>) = colecao_aleatoria
+    let is_all_even1 = all_evens
+        .clone()
         .into_iter()
-        .partition(|&n| n % 2 == 0);
+        .all(|x| x % 2 == 0);
 
-    println!("{:?}", colecao_aleatoria);
-    println!("{:?}", pares);
-    println!("{:?}", impares);
+    let is_all_even2 = not_all_evens
+        .clone()
+        .into_iter()
+        .all(|x| x % 2 == 0);
+
+    println!("{:?}", all_evens);
+    println!("{:?}", not_all_evens);
+    println!("{:?}", is_all_even1);
+    println!("{:?}", is_all_even2);
 }
