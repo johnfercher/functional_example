7aa1ff5f9aa15883daaa25b1091e98f50025a168
John Fercher <johnathanfercher22@gmail.com>
2019-04-10 14:26:23 -0300

    unzip

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ extern crate rand;
 fn main() {
     let tuples_array = [(1, 2), (3, 4)];
 
-    let (left, right) : (Vec<u16>, Vec<u16>) = tuples_array.iter().cloned().unzip();
+    let (left, right) : (Vec<u16>, Vec<u16>) = tuples_array
+        .iter()
+        .cloned()
+        .unzip();
 
     println!("{:?}", tuples_array);
     println!("{:?}", left);
