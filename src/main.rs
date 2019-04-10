c4d3dac28afc44fce85fa74dec36366cf71d6fa3
John Fercher <johnathanfercher22@gmail.com>
2019-04-10 01:09:16 -0300

    zip

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 extern crate rand;
 
 fn main() {
-    let colecao_aleatoria: [u16; 8] = rand::random();
+    let numbers = vec![1,2,3,4,5,6];
+    let numbers2 = vec![1,2,3,4,5,6];
 
-    let max = colecao_aleatoria
+    let contains_any_umatched_numbers = numbers
         .iter()
-        .max()
-        .unwrap();
+        .zip(numbers2.iter())
+        .map(|(num1, num2)| num1 == num2)
+        .collect::<Vec<bool>>()
+        .contains(&false);
 
-    println!("{:?}", colecao_aleatoria);
-    println!("{:?}", max);
+    println!("{:?}", numbers);
+    println!("{:?}", numbers2);
+    println!("{:?}", contains_any_umatched_numbers);
 }
