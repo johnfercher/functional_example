680083356622ca8212840e827ddb82ec14c321c0
John Fercher <johnathanfercher22@gmail.com>
2019-04-10 15:16:50 -0300

    zip

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,17 @@ extern crate rand;
 
 fn main() {
     let numbers = vec![1,2,3,4,5,6];
-    let numbers2 = vec![1,2,3,4,5,6];
+    let numbers2 = vec![6,5,4,3,2,1];
 
-    let contains_any_umatched_numbers = numbers
-        .iter()
-        .zip(numbers2.iter())
-        .map(|(num1, num2)| num1 == num2)
-        .collect::<Vec<bool>>()
-        .contains(&false);
+    let tuples = numbers
+        .clone()
+        .into_iter()
+        .zip(numbers2
+            .clone()
+            .into_iter())
+        .collect::<Vec<(u16, u16)>>();
 
     println!("{:?}", numbers);
     println!("{:?}", numbers2);
-    println!("{:?}", contains_any_umatched_numbers);
+    println!("{:?}", tuples);
 }
