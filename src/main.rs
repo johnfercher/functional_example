3afea0fadccac5fa1b9ce7b2fc924bcfd180959b
John Fercher <johnathanfercher22@gmail.com>
2019-04-09 23:11:48 -0300

    map

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate rand;
+
 fn main() {
-    println!("Hello, world!");
+    let colecao_aleatoria: [u16; 32] = rand::random();
+
+    let numbers = colecao_aleatoria
+        .to_vec()
+        .into_iter()
+        .map(|x| {
+            if x % 2 == 0 {
+                String::from(format!("Número par: {:?}", x))
+            } else {
+                String::from(format!("Número impar: {:?}", x))
+            }
+        })
+        .collect::<Vec<String>>();
+
+    println!("{:?}", colecao_aleatoria);
+    println!("{:?}", numbers);
 }
